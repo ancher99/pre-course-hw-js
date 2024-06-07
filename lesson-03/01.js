afc9dd56ee701c5760586ca932db86289a248ea6
anton <antonchervyakov2003@bk.ru>
2024-06-07 13:53:33 +0300

    complete 03/01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((v) =>{
+        if (v ==='черный' || v ==='красный'|| v ==='желтый'){
+            return v
+        }
+    }).join('-')
+}
+
+console.log(createColorString());
